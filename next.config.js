39fb2f1fba6da5209e842603b6343ccf0aa4fb5e
JoshuaGumahad <gumahadjoshua@gmail.com>
2024-10-07 14:38:13 +0800

    update today

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['localhost'],
+      domains: ['localhost', '192.168.0.108'],
       remotePatterns: [
         {
           protocol: 'http',
           hostname: 'localhost',
           port: '',
-          pathname: '/recipeshare-app-1/assets/images/**',
+          pathname: '/recipewebv3/assets/images/**',
         },
       ],
     },
